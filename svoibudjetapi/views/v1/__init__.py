7e8c44ea4175c819997c45d4dca842e979d84663
Petrov Vladimir <neoman36@gmail.com>
2018-11-08 13:06:55 +0300

    Route for products was added

--- svoibudjetapi/views/v1/__init__.py
+++ svoibudjetapi/views/v1/__init__.py
@@ -1,9 +1,11 @@
 from . import (
     checks,
+    products,
     qr_strings,
 )
 
 __all__ = [
     'checks',
+    'products',
     'qr_strings',
 ]
---
+++ svoibudjetapi/views/v1/products.py
@@ -0,0 +1,52 @@
+from flask import (
+    jsonify,
+    request,
+)
+from sqlalchemy import sql
+from svoibudjetapi import app, db
+from svoibudjetapi.models import Product, Item
+from svoibudjetapi.support import generate_joins, get_sort_by_rule
+
+
+@app.route('/v1/products', methods=['GET'])
+def get_products():
+    queryset = Product.query.options(*generate_joins(request.args.get('include', ''), Product))
+    offset = request.args.get('offset', 0, int)
+    limit = request.args.get('limit', 10, int)
+
+    if 'sort_by' in request.args:
+        field, direction = get_sort_by_rule(request.args['sort_by'])
+        if field == 'min_price':
+            min_price_subquery = db.session\
+                .query(Item.product_id, sql.label('min_price', db.func.min(Item.price)))\
+                .group_by(Item.product_id)\
+                .subquery()
+
+            queryset = queryset.join(min_price_subquery)\
+                .order_by(min_price_subquery.c.min_price)\
+                .with_entities(Product, min_price_subquery)
+        else:
+            try:
+                sort_by_rule = getattr(getattr(Product, field), direction)()
+            except AttributeError:
+                return jsonify({
+                    'message': f'Invalid value for sort_by. Must valid field name or field name with prefix -/+.'
+                }), 400
+
+            queryset = queryset.order_by(sort_by_rule)
+
+    if 'name_contains' in request.args:
+        queryset = queryset.filter(Product.name.contains(request.args['name_contains'].strip()))
+
+    def transformer(item):
+        if isinstance(item, tuple):
+            item, item_id, min_price = item
+            item = item.to_dict()
+            item['min_price'] = float(min_price)
+        return item
+
+    data = {
+        'total_count': queryset.count(),
+        'items': [transformer(i) for i in queryset[offset:offset + limit]],
+    }
+    return jsonify(data)
