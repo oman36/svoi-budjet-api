a58794c30df4a246c6c9bcd45182ccb2bb6f6d9e
Petrov Vladimir <neoman36@gmail.com>
2018-11-08 14:05:32 +0300

    Product items route done

--- svoibudjetapi/views/v1/products.py
+++ svoibudjetapi/views/v1/products.py
@@ -1,11 +1,22 @@
 from flask import (
     jsonify,
     request,
+    abort,
 )
 from sqlalchemy import sql
-from svoibudjetapi import app, db
-from svoibudjetapi.models import Product, Item
-from svoibudjetapi.support import generate_joins, get_sort_by_rule
+from svoibudjetapi import (
+    app,
+    db,
+)
+from svoibudjetapi.models import (
+    Product,
+    Item,
+)
+from svoibudjetapi.support import (
+    generate_joins,
+    get_sort_by_rule,
+    get_eval_sort_by_rule,
+)
 
 
 @app.route('/v1/products', methods=['GET'])
@@ -50,3 +61,30 @@ def get_products():
         'items': [transformer(i) for i in queryset[offset:offset + limit]],
     }
     return jsonify(data)
+
+
+@app.route('/v1/products/<int:id_>/items', methods=['GET'])
+def get_product_items(id_):
+    if Product.query.filter_by(id=id_).count() == 0:
+        return abort(404)
+
+    offset = request.args.get('offset', 0, int)
+    limit = request.args.get('limit', 100, int)
+
+    queryset = Item.query.filter_by(product_id=id_)\
+        .options(*generate_joins(request.args.get('include', ''), Item))
+
+    if 'sort_by' in request.args:
+        try:
+            sort_by_rule = get_eval_sort_by_rule(request.args['sort_by'], Item)
+        except AttributeError:
+            return jsonify({
+                'message': f'Invalid value for sort_by. Must valid field name or field name with prefix -/+.'
+            }), 400
+
+        queryset = queryset.order_by(sort_by_rule)
+
+    return jsonify({
+        'total_count': queryset.count(),
+        'items': queryset[offset:limit + offset],
+    })
