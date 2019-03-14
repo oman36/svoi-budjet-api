5f783747250d1b3f12e6c6a53a200e043d52a56d
Petrov Vladimir <neoman36@gmail.com>
2019-03-14 11:37:23 +0300

    Env settings was moved to the file

--- svoibudjetapi/__init__.py
+++ svoibudjetapi/__init__.py
@@ -1,14 +1,33 @@
 import logging.config
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from svoibudjetapi.support import CustomJSONEncoder
 
+log_msgs = []
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+try:
+    app.config.from_pyfile('env_config.py')
+except FileNotFoundError:
+    log_msgs.append((logging.WARN, 'File env_config.py not found.'))
 app.json_encoder = CustomJSONEncoder
-logging.config.dictConfig(app.config.get('LOGGING', {'version': 1}))
+
+if not os.path.exists(app.config['LOG_DIR']):
+    log_msgs.append((logging.WARN, '%s was created for logs.', app.config['LOG_DIR']))
+    os.makedirs(app.config['LOG_DIR'])
+
+if 'LOGGING' in app.config:
+    logging.config.dictConfig(app.config['LOGGING'])
+else:
+    logging.basicConfig()
+
+logger = logging.getLogger(__name__)
+for args in log_msgs:
+    logger.log(*args)
+
 db = SQLAlchemy(app)
 
 import svoibudjetapi.views
